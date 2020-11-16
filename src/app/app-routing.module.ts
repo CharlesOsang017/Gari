93291f74db74bcf7bb4fb9584bafdbad0e4f0ebf
CharlesOsang017 <charlesosango02@gmail.com>
2020-11-16 23:32:57 +0300

    Add testimonial section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// import { BlogsComponent } from './blogs/blogs.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ServiceOfferedComponent } from './service-offered/service-offered.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path: 'bllogs', component: BlogsComponent},
+  {path: '', component: LandingPageComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'services', component: ServiceOfferedComponent},
+  {path: 'signup', component: SignupComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ServiceOfferedComponent } from './service-offered/service-offered.component';
+import { SignupComponent } from './signup/signup.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogsComponent,
+    LoginComponent,
+    NavbarComponent,
+    ProfileComponent,
+    ServiceOfferedComponent,
+    SignupComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
