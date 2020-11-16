8de651a5b4fbb3f18e6d770e3e4dc063e9903969
CharlesOsang017 <charlesosango02@gmail.com>
2020-11-16 10:27:51 +0300

    Folder structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogsComponent } from './blogs/blogs.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ServiceOfferedComponent  } from './service-offered/service-offered.component';
 
-const routes: Routes = [];
+import { Observable } from 'rxjs';
+const routes: Routes = [
+  {path: 'Signup', component: SignupComponent },
+  {path: 'Login', component: LoginComponent },
+  {path: 'Profile', component: ProfileComponent },
+  {path: 'Blog', component: BlogsComponent },
+  {path: 'Service', component: ServiceOfferedComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceOfferedComponent } from './service-offered/service-offered.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServiceOfferedComponent,
+    BlogsComponent,
+    LoginComponent,
+    SignupComponent,
+    ProfileComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
