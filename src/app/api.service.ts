111a829ff44af4dff62cea189f6c6819f2abd837
kelvin-daniel <kaymutor@gmail.com>
2020-11-22 23:38:00 +0300

    import httpclient and observables

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private) { }
+  baseurl = "gariworkshop.herokuapp.com"
+  httpHeaders = new HttpHeaders({'Content-type': 'application/json'})
+
+  constructor(private http:HttpClient) { }
+
+  getAllServices(): Observable<any>{
+    return this.http.get(this.baseurl + '/services/')
+  }
 }
